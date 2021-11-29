acfcbd2044e98e5882e863701c73a144be036fbd
nihong.wu <nihong.wu@uama.com.cn>
2021-11-29 18:18:27 +0800

    save

--- vue/my-router/src/myRouter.js
+++ vue/my-router/src/myRouter.js
@@ -2,39 +2,45 @@ let Vue
 class VueRouter {
   constructor(options) {
     this.$options = options
-    this.current = '/'
-    window.addEventListener('hashchange', function() {
+    // 将current变成响应式的，router-view能够重新渲染
+    const initial = window.location.hash.slice('#') || '/'
+    Vue.util.defineReactive(this, 'current', initial)
+    window.addEventListener('hashchange', function () {
       this.current = window.location.hash.slice(1)
     })
   }
 }
 
-VueRouter.install = function(_vue) {
+VueRouter.install = function (_vue) {
   Vue = _vue
   Vue.mixin({
-    beforeCreate() {
-      Vue.prototype.$router = this.$options
+    beforeCreate () {
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
     },
   })
   Vue.component('router-link', {
-    props:{
-      to:{
+    props: {
+      to: {
         type: String,
         required: true
       }
     },
-    render(h) {
-      return h('a', {attrs: {href: '#'+ this.to }}, this.$slots.default)
+    render (h) {
+      return h('a', { attrs: { href: '#' + this.to } }, this.$slots.default)
     }
   })
   Vue.component('router-view', {
-    render(h) {
+    render (h) {
       let component
+      console.log(this)
       const route = this.$router.$options.routes.find(route => route.path === this.$router.current)
-      if(route) {
+      if (route) {
         component = route.component
       }
       return h(component)
+
     }
   })
 }
