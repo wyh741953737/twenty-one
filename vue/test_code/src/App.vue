596bbefa081ca6cd2e7162b6be8d5cba6647a341
nihong.wu <nihong.wu@uama.com.cn>
2021-12-30 09:25:10 +0800

    save

---
+++ vue/test_code/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => {
+    return {
+      name: 'h哈哈'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
