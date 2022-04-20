cbe02cf2c0e2a1acd72df368ce64bc397f1c3686
nihong.wu <741953737@qq.com>
2022-04-20 15:49:02 +0800

    save

--- vue/my-router/src/components/About.vue
+++ vue/my-router/src/components/About.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="hello">
     About
-    <p @click="$store.commit('add')">count: {{$store.state.counter}}</p>
-    <p @click="$store.dispatch('asyncAdd')">asyncCount: {{$store.state.age}}</p>
-    <p>我是getters---{{$store.getters.doubleCount}}</p>
-    <p>我是另一个getters---{{$store.getters.doubleDoubleCount}}</p>
   </div>
 </template>
 
