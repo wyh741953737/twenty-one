88a8c909079662bd7b12882cccbd2ec84149f748
nihong.wu <741953737@qq.com>
2022-06-13 19:44:49 +0800

    save

--- vue/snabbdom/src/index.js
+++ vue/snabbdom/src/index.js
@@ -1,7 +1,39 @@
-import h from './mysnabbdom/h'
+// import h from './mysnabbdom/h'
 
-const myVnode = h('ul', {}, [
-  h('li', {}, '苹果'),
-  h('li', {}, h('div', {}, '我是香蕉')),
+// const myVnode = h('ul', {}, [
+//   h('li', {}, '苹果'),
+//   h('li', {}, h('div', {}, '我是香蕉')),
+// ])
+// console.log('----', myVnode)
+
+import {
+  init,
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule,
+  h,
+} from "snabbdom";
+
+const patch = init([classModule, propsModule, styleModule, eventListenersModule]);
+
+const vnode1 = h('ul', {}, [
+  h('li', {}, 'A'),
+  h('li', {}, 'B'),
+  h('li', {}, 'C'),
+  h('li', {}, 'D')
 ])
-console.log('----', myVnode)
+
+const vnode2 = h('ul', {}, [
+  h('li', {}, 'A'),
+  h('li', {}, 'B'),
+  h('li', {}, 'C'),
+  h('li', {}, 'D'),
+  h('li', {}, 'E')
+])
+
+const container = document.getElementById('container')
+patch(container, vnode1)
+btn.onclick = function () {
+  patch(vnode1, vnode2)
+}
--- vue/snabbdom/src/mysnabbdom/h.js
+++ vue/snabbdom/src/mysnabbdom/h.js
@@ -1,31 +1,28 @@
 import vnode from './vnode'
 
-// h('div')
-// h('div', '文字')
-// h('div', {}, [])
-// h('div', {}, [
-  // h('li', '苹果'),
-  // h('li', '榴莲'),
-  // h('li', '西瓜'),
-  // h('li', '香蕉'),
+//  h('ul', {}, [
+//   h('li', {}, '苹果'),
+//   h('li', {}, h('div', {}, '我是香蕉')),
 // ])
-// h('div', [])
 // 函数重载
 export default function (sel, data, c) {
-  console.log(c)
+  if(arguments.length < 3) {
+    alert('参数必须为3个')
+    return
+  }
    if(typeof c === 'string' || typeof c === 'number') {
      return vnode(sel, data, undefined, c, undefined)
    } else if(Array.isArray(c)) {
-    for(let i = 0; i < c.length; c++) {
-      let children = []
-      if(typeof c[i] === 'object' && c[i].hasOwnProperty('sel')) {
+    let children = []
+    for(let i = 0; i < c.length; i++) {
+      if(typeof c[i] === 'object' && !c[i].hasOwnProperty('sel')) {
         throw new Error('传入的数组参数中有一项不是h函数')
-      } 
+      }
       children.push(c[i])
+      console.log(children)
     }
     return vnode(sel, data, children, undefined, undefined)
    } else if(typeof c === 'object' && c.hasOwnProperty('sel')) {
-    
    } else {
      throw new Error('传入的第三个参数类型不对')
    }
